b777daa6fa95848884a5a86a16ff9423aaa9f250
parksamu <samujpark@gmail.com>
2018-03-10 15:48:54 -0600

    Charts added and styled

---
+++ src/_scripts/_charts.js
@@ -0,0 +1,56 @@
+var Plotly = require('plotly.js/lib/core');
+var Plotlybar = require('plotly.js/lib/bar');
+
+Plotly.register(Plotlybar);
+
+// At the end of the _charts.js file
+console.log('Will, is that you?')
+console.log(annualTotals);
+
+
+// Initialize the arrays that will hold our lists of data
+var countyHomicides = annualTotals.map(a => a.homicides_total);
+var harvardParkHomicides = annualTotals.map(a => a.homicides_harvard_park);
+var years = annualTotals.map(a => a.year);
+
+function createChart(x,y,element, title){
+  var settings = [{
+    x: x,
+    y: y,
+    type: 'bar',
+    marker: {
+      color:  '#86c7df'
+    },
+    hoverinfo: 'y',
+    hoverlabel: {
+bgcolor: '#777777'
+    }
+  }];
+
+  var layout = {
+    title: title,
+    xaxis:{
+      title: 'Year',
+      fixedrange: true
+    },
+    yaxis:{
+      fixedrange: true
+    },
+    margin: {
+      l: 30,
+      r: 15,
+      t: 45,
+      b: 30
+    },
+    height: 250
+  };
+
+  //create the Charts
+  Plotly.newPlot(element, settings, layout, {displayModeBar: false});
+}
+
+var countyChartTitle = "<b>County Homicides, 2000-2017 </b>"
+var hpChartTitle = "<b>Harvard Park Homicides, 2000-2017 </b>"
+
+createChart(years, countyHomicides, 'county-homicides', countyChartTitle);
+createChart(years, harvardParkHomicides, 'harvard-park-homicides', hpChartTitle)
